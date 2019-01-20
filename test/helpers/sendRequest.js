b5201d8c9fe750bc4dcfc1ba3e997a470cd0a43c
Aman <aman.mender@tegningtil3d.no>
2019-01-20 10:49:11 +0100

    Created working mocha tests

--- test/helpers/sendRequest.js
+++ test/helpers/sendRequest.js
@@ -21,6 +21,29 @@ sendRequest = (url,cb) => {
           }
         });
     });    
+};
+sendRequestPromise = (url) => {
+    return new Promise(function(resolve,reject) {        
+        http.get(url || "http://localhost:2024/god-dag",(res) => {
+            const { statusCode } = res;
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+              try {
+                const parsedData = JSON.parse(rawData);
+                resolve({
+                    data: parsedData,
+                    statusCode: statusCode
+                })
+              } catch (e) {
+                    console.error(e.message);
+                    reject(e);
+              }
+            });
+        });    
+    })
 }
 
-module.exports = sendRequest;
+module.exports.sendRequest = sendRequest;
+module.exports.promiseRequest = sendRequestPromise;
--- test/test.js
+++ test/test.js
@@ -2,4 +2,90 @@ const assert = require('assert');
 const chai = require('chai');
 const chaiAssert = chai.assert;
 
-describe('Parse request with cookie', function() {});
+const fs = require('fs');
+const http = require('http');
+const Direction = require('../lib/index.js');
+
+const {sendRequest,promiseRequest} = require('./helpers/sendRequest');
+
+/*let file = fs.readFileSync("./test/helpers/website.html","utf-8");
+console.log(typeof file,file);*/
+
+let DirectionRoutes = [
+    {
+        name: "god-dag",
+        method: "GET",
+        handler: (req,res) => {
+            let message = JSON.stringify({"message":"Hei, ha en god dag!"});
+            res.end(message);
+        }
+    },
+    {
+        name: "hello",
+        method: "GET",
+        handler: (req,res) => {
+            let message = JSON.stringify({"message":"Hello, dude. You are great!"});
+            res.end(message);
+        }
+    },
+    {
+        name: "website",
+        method: "GET",
+        handler: (req,res) => {
+            let body = "<html><h2>Hello</h2></html>"
+            //res.setHeader()
+            res.end(body);
+        }
+    },
+    {
+        name: "mysite",
+        method: "GET",
+        handler: (req,res) => {
+            let file = fs.readFileSync("./test/helpers/website.html","utf-8");
+            console.log(typeof file,file);
+            //let body = "<html><h2>Hello</h2></html>"
+            //res.setHeader()
+            res.end(file);
+        }
+    },
+    {
+        name: "bootstrap",
+        method: "GET",
+        handler: (req,res) => {
+            let file = fs.readFileSync("./test/helpers/bootstrap.html","utf-8");
+            res.end(file);
+        }
+    }
+]
+
+const DirectionRouter = new Direction(DirectionRoutes);
+
+const server  = http.createServer((req,res) => {
+    DirectionRouter.handleRequests(req,res)
+});
+
+let port = process.env.PORT || '2024';
+server.listen(port,function() {
+    console.log(`The server is listening on ${port}`)
+});
+
+describe('Basic json request', function() {
+    describe('Test first endpoint', function() {
+        it('correct statusCode and json', function() {
+            sendRequest("http://localhost:2024/god-dag",function(e,r) {
+                assert.equal(r.statusCode, 200);
+                assert.equal(r.data.message, "Hei, ha en god dag!");
+            });
+        })
+    })
+    describe('Test second endpoint', function() {
+        it('correct statusCode and json', function() {
+            sendRequest("http://localhost:2024/hello",function(e,r) {
+                assert.equal(r.statusCode, 200);
+                assert.equal(r.data.message, "Hello, dude. You are great!");
+            })
+        })
+    });
+})
+
+//describe('Basic html request', function() {});
