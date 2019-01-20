c068352237024b482c0d5d3fb280011cb152990b
Aman <aman.mender@tegningtil3d.no>
2019-01-20 11:01:07 +0100

    Added webpack build

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './lib/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    plugins: [
+        new MinifyPlugin({},{})
+    ]
+};
